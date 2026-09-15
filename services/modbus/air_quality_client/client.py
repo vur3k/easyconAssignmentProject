9ebd7cd8bdfb450669f3866aee34e6353b99fe4f
Václav Přibyl <525092@mail.muni.cz>
2026-09-15 21:05:15 +0200

    Implemented client-server communication using modbustcp protocol

---
+++ services/modbus/air_quality_client/client.py
@@ -0,0 +1,136 @@
+import logging
+import os
+import time
+import asyncio
+
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+from pymodbus.client import ModbusTcpClient
+
+logging.basicConfig(level=logging.INFO)
+
+MODBUS_HOST = os.environ["MODBUS_HOST"]
+MODBUS_PORT = int(os.environ["MODBUS_PORT"])
+MODBUS_DEVICE_ID = int(os.environ.get("MODBUS_DEVICE_ID", "1"))
+MODBUS_POLL_INTERVAL = int(os.environ["MODBUS_POLL_INTERVAL"])
+
+REGISTER_COUNT = 6
+
+MONGO_HOST = os.environ["MONGO_HOST"]
+MONGO_PORT = int(os.environ["MONGO_PORT"])
+MONGO_DATABASE = os.environ["MONGO_DATABASE"]
+MONGO_COLLECTION = os.environ["MONGO_COLLECTION"]
+
+
+def connect_to_db():
+    while True:
+        try:
+            client = MongoClient(
+                host=MONGO_HOST,
+                port=MONGO_PORT,
+                serverSelectionTimeoutMS=5000,  # 5 seconds timeout
+            )
+            client.admin.command("ping")  # Check if the server is available
+            logging.warning(
+                "Connected to MongoDB at %s:%s",
+                MONGO_HOST,
+                MONGO_PORT
+            )
+            return client
+        except ConnectionFailure:
+            logging.error(
+                "Failed to connect to MongoDB at %s:%s. Retry in 5 seconds...",
+                MONGO_HOST,
+                MONGO_PORT
+            )
+            time.sleep(5)
+
+
+def save_reading(client: MongoClient, reading: dict):
+    collection = client[MONGO_DATABASE][MONGO_COLLECTION]
+    result = collection.insert_one(reading)
+    logging.info(f"Stored reading with id {result.inserted_id} {reading}")
+
+
+async def read_air_quality() -> dict | None:
+    client = ModbusTcpClient(
+        host=MODBUS_HOST,
+        port=MODBUS_PORT,
+        timeout=15,
+    )
+    try:
+        if not client.connect():
+            logging.error(
+                "Cannot connect to Modbus server at %s:%s",
+                MODBUS_HOST,
+                MODBUS_PORT,
+            )
+            return None
+
+        response = client.read_input_registers(
+            address=0,
+            count=REGISTER_COUNT,
+            slave=MODBUS_DEVICE_ID,
+        )
+
+        if response.isError():
+            logging.error(
+                "Modbus read failed: %s",
+                response,
+            )
+            return None
+
+        registers = response.registers
+
+        if len(registers) != REGISTER_COUNT:
+            logging.error(
+                "Expected %d registers, got %d",
+                REGISTER_COUNT,
+                len(registers),
+            )
+            return None
+
+        logging.info(
+            "Raw registers: %s",
+            registers,
+        )
+
+        return {
+            "pm2_5_ug_m3": registers[0] / 10.0,
+            "pm10_ug_m3": registers[1] / 10.0,
+            "nitrogen_dioxide_ug_m3": registers[2] / 10.0,
+            "ozone_ug_m3": registers[3] / 10.0,
+            "european_aqi": registers[4],
+            "data_valid": bool(registers[5]),
+        }
+
+    except Exception:
+        logging.exception(
+            "Unexpected Modbus exception"
+        )
+        return None
+
+    finally:
+        client.close()
+
+
+async def async_loop():
+    mongo_client = connect_to_db()
+
+    try:
+        while True:
+            reading = await read_air_quality()
+            if reading:
+                save_reading(mongo_client, reading)
+            else:
+                logging.warning("Couldn't save malformed data")
+
+            logging.info(time.time())
+            await asyncio.sleep(MODBUS_POLL_INTERVAL)
+    except KeyboardInterrupt:
+        logging.info("Stopping Modbus Client.")
+    finally:
+        mongo_client.close()
+
+if __name__ == "__main__":
+    asyncio.run(async_loop())
---
+++ services/modbus/air_quality_server/server.py
@@ -0,0 +1,116 @@
+import asyncio
+import logging
+import requests
+import os
+
+from pymodbus.datastore import (
+    ModbusSequentialDataBlock,
+    ModbusServerContext,
+    ModbusSlaveContext
+)
+from pymodbus.server import StartAsyncTcpServer
+
+logging.basicConfig(level=logging.INFO)
+
+MODBUS_HOST = os.environ.get("MODBUS_HOST", "0.0.0.0")
+MODBUS_PORT = int(os.environ.get("MODBUS_PORT", "5020"))
+
+LATITUDE = float(os.environ["LATITUDE"])
+LONGITUDE = float(os.environ["LONGITUDE"])
+UPDATE_INTERVAL = int(os.environ["UPDATE_INTERVAL"])
+
+API_URL = os.environ["OPEN_METEO_API_URL"]
+API_PARAMS = {
+    "latitude": LATITUDE,
+    "longitude": LONGITUDE,
+    "current": "pm2_5,pm10,nitrogen_dioxide,ozone,european_aqi"
+}
+
+INITIAL_INPUT_REGISTERS = [
+    87,   # PM2.5: 8.7 µg/m³
+    142,  # PM10: 14.2 µg/m³
+    116,  # NO2: 11.6 µg/m³
+    742,  # O3: 74.2 µg/m³
+    32,   # European AQI
+    1,    # Data valid
+]
+
+
+def create_context():
+    input_registers = ModbusSequentialDataBlock(
+        1,
+        INITIAL_INPUT_REGISTERS,
+    )
+
+    slave_context = ModbusSlaveContext(
+        ir=input_registers,
+    )
+
+    server_context = ModbusServerContext(
+        slaves=slave_context,
+        single=True,
+    )
+
+    return server_context, input_registers
+
+
+def extract_fetched_data(data: dict[str, any]) -> list[int]:
+    return [
+        int(data.get("pm2_5", 0)) * 10,
+        int(data.get("pm10", 0)) * 10,
+        int(data.get("nitrogen_dioxide", 0)) * 10,
+        int(data.get("ozone", 0)) * 10,
+        int(data.get("european_aqi", 0)),
+        1
+    ]
+
+
+def fetch_air_quality():
+    try:
+        response = requests.get(API_URL, params=API_PARAMS, timeout=15)
+        response.raise_for_status()
+        data = response.json()
+        current = data.get("current", {})
+        if not current:
+            logging.warning("Malformed API response: %s", data)
+            return None
+        return extract_fetched_data(current)
+    except Exception as e:
+        logging.error(
+            "Failed to fetch air quality data: %s",
+            e
+        )
+        return None
+
+
+async def update_registers_task(input_registers: ModbusSequentialDataBlock):
+    while True:
+        new_data = fetch_air_quality()
+        await input_registers.async_setValues(
+            1,
+            new_data,
+        )
+        logging.info("Updated registers: %s", new_data)
+        await asyncio.sleep(5)
+
+
+async def async_loop():
+    context, input_registers = create_context()
+    asyncio.create_task(
+        update_registers_task(input_registers)
+    )
+
+    logging.info(
+        "Starting air-quality Modbus TCP server on %s:%s",
+        MODBUS_HOST,
+        MODBUS_PORT,
+    )
+
+    await StartAsyncTcpServer(
+        context=context,
+        address=(MODBUS_HOST, MODBUS_PORT),
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(async_loop())
